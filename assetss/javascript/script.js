944492286d21dadf43bc19d646002af2803ff35a
Biecho88 <>
2023-11-01 21:51:09 +0000

    Removed each tile divs as this will be done through javascript and added comments to variables

--- assetss/javascript/script.js
+++ assetss/javascript/script.js
@@ -1,6 +1,7 @@
-const tileContainer = document.querySelector(".tiles") ;
-const emoji = ["&#x1F412", "&#x1F41E", "&#x1F427", "&#x1F438", "&#x1F980", "&#x1F984", "&#x1F98B", "&#x1F99E", "&#x1F9A9", "&#x1F436", "&#x1F680", "&#x1F681", "&#x1F682", "&#x1F692", "&#x1F693", "&#x1F69C", "&#x1F6A2", "&#x1F60D", "&#x1F621", "&#x1F608"] ;
-const emojiPicklist = [...emoji , ...emoji] ;
-const tileCount = emojiPicklist.length ; 
+const tileContainer = document.querySelector(".tiles") ; // variable targeting div with class .tiles
+const emoji = ["&#x1F412", "&#x1F41E", "&#x1F427", "&#x1F438", "&#x1F980", "&#x1F984", "&#x1F98B", "&#x1F99E", "&#x1F9A9", "&#x1F436", "&#x1F680", "&#x1F681", "&#x1F682", "&#x1F692", "&#x1F693", "&#x1F69C", "&#x1F6A2", "&#x1F60D", "&#x1F621", "&#x1F608"] ; // array list with 20 emoji's
+const emojiPicklist = [...emoji , ...emoji] ; // variable doubling the emoji count as each emoji is needed twice
+const tileCount = emojiPicklist.length ; //defining number of tiles for the game
 
-console.log(emojiPicklist) ;
+// Variables indicating current state of the game
+
