d88eebd2e9932cfc1900b919313ee8414ce0b205
Biecho88 <>
2023-11-01 21:16:31 +0000

    Added JavaScript link to the index.html and started to create const variubles targeting .tiles class and containing emoji's array

---
+++ assetss/javascript/script.js
@@ -0,0 +1,5 @@
+const tileContainer = document.querySelector(".tiles") ;
+const emoji = ["&#x1F412", "&#x1F41E", "&#x1F427", "&#x1F438", "&#x1F980", "&#x1F984", "&#x1F98B", "&#x1F99E", "&#x1F9A9", "&#x1F436", "&#x1F680", "&#x1F681", "&#x1F682", "&#x1F692", "&#x1F693", "&#x1F69C", "&#x1F6A2", "&#x1F60D", "&#x1F621", "&#x1F608"] ;
+const emojiPicklist = [...emoji , ...emoji] ;
+
+console.log(emojiPicklist) ;
