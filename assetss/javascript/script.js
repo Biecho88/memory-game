c468efbd5dc1949fd4bec6ac9bd5a59a1434cdc7
Biecho88 <>
2023-11-14 22:42:54 +0000

    Removed buttons and added timer and moves counter.  Time now starts imidietly after page is refreshed and cout dosnt work yet

--- assetss/javascript/script.js
+++ assetss/javascript/script.js
@@ -2,6 +2,36 @@ const tilesContainer = document.querySelector(".tiles");    // variable targetin
 const emojis = ["&#x1F412", "&#x1F41E", "&#x1F427", "&#x1F438", "&#x1F980", "&#x1F984", "&#x1F98B", "&#x1F99E", "&#x1F9A9", "&#x1F436", "&#x1F680", "&#x1F681", "&#x1F682", "&#x1F692", "&#x1F693", "&#x1F69C", "&#x1F6A2", "&#x1F60D", "&#x1F621", "&#x1F608"];    // array list with 20 emoji's
 const emojiPicklist = [...emojis, ...emojis];   // variable doubling the emoji count as each emoji is needed twice
 const tileCount = emojiPicklist.length;    //defining number of tiles for the game
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+
+//Initial Time
+let seconds = 0,
+    minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+    winCount = 0;
+//For timer
+const timeGenerator = () => {
+    seconds += 1;
+    //minutes logic
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+    //format time before displaying
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+//For calculating moves
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+
 
 // Variables indicating current state of the game
 
@@ -70,6 +100,8 @@ function buildTile(emoji) {
 
     return element;
 }
+
+
 // Build up tiles 
 
 for (let i = 0; i < tileCount; i++) {
@@ -83,3 +115,8 @@ for (let i = 0; i < tileCount; i++) {
 
 
 }
+
+interval = setInterval(timeGenerator, 1000);
+//initial moves
+moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+initializer();
